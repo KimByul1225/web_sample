3f7f519e965e4ac3d026f3ce0ae479da9086a23c
ByulKim <kimbyul1225@gmail.com>
2023-04-25 20:20:15 +0900

    불필요 파일 정리 및 router 세팅

---
+++ src/router.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { createBrowserRouter } from "react-router-dom";
+import App from './App';
+
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "",
+                element: <App />,
+            }
+        ]
+    }
+])
+
+export default router
