4f04e85e4fadd391d1ddbe616d48fca37144db4a
ByulKim <kimbyul1225@gmail.com>
2023-05-13 19:19:43 +0900

    메인페이지 슬라이더 타입3 ui작업 완료

--- src/components/home/SliderType03.tsx
+++ src/components/home/SliderType03.tsx
@@ -2,11 +2,11 @@ import React, { useRef } from 'react';
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
 import SectionTitle from '../commons/SectionTitle';
-
 import { Swiper, SwiperSlide } from 'swiper/react';
 import "swiper/css";
 import "swiper/css/pagination";
 import { Pagination } from "swiper";
+import arrowIcon from "@/resources/icons/home/icon_slider_type03_arrow.png"
 
 
 const SliderType03 = () => {
@@ -51,36 +51,30 @@ const SliderType03 = () => {
                         className="type03_box"
                     > 
                         <BoxTitle>타이틀</BoxTitle>
-
-
-
                         <ListWrap>
                             <li>
-                                <span className='esglist_tit'>종목코드</span>
+                                <p className='esglist_tit'>종목코드</p>
                                 <span className='esglist_txt'>종목코드56151561</span>
                             </li>
                             <li>
-                                <span className='esglist_tit'>규모등급</span>
+                                <p className='esglist_tit'>규모등급</p>
                                 <span 
                                     
                                 >51651</span>
                             </li>
                             <li>
-                                <span className='esglist_tit'>자산규모</span>
+                                <p className='esglist_tit'>자산규모</p>
                                 <span className='esglist_txt'>자산규모515616</span>
                             </li>
                             <li>
-                                <span className='esglist_tit'>전체등급</span>
+                                <p className='esglist_tit'>전체등급</p>
                                 <span>
                                     4111651
                                 </span>
 
                             </li>
                         </ListWrap>
-
-                        <Link to="/" className="esg_more">바로가기</Link>
-
-
+                        <MoreView to="/">바로가기</MoreView>
                     </SwiperSlide>
 
 
@@ -162,25 +156,94 @@ const BoxTitle = styled.h4`
     }
 `
 
+
 const ListWrap = styled.ul`
     display: flex; flex-wrap: wrap; justify-content: space-between;
     li{
         display: flex; align-items: center;
+        p{
+            display: inline-block;
+            font: var(--b1);
+        }
+        span{
+            display: inline-block;
+            font: var(--b1); font-weight: 700;
+        }
     }
     li:nth-child(odd){
         width: 50%;
+        p{
+            width: 50%;
+        }
     }
     li:nth-child(even){
         width: 37%;
+        p{
+            width: 70%;
+        }
     }
+    li:nth-child(3), li:nth-child(4){
+        margin-top: 20px;
+    }
+
     @media screen and (max-width: 1200px) {
         flex-direction: column;
+        li{
+            margin-top: 20px;
+            p{
+                font-size: 18px;
+            }
+            span{
+                font-size: 18px;
+            }
+        }
+        li:nth-child(odd){
+            width: 70%;
+        }
+        li:nth-child(even){
+            width: 70%;
+            p{
+                width: 50%;
+            }
+        }
+        
     }
     @media screen and (max-width: 768px) {
+        li{
+            margin-top: 30px;
+            p{
+                font-size: 16px;
+            }
+            span{
+                font-size: 16px;
+            }
+        }
+        li:nth-child(3), li:nth-child(4){
+            margin-top: 30px;
+        }
+        li:nth-child(odd), li:nth-child(even){
+            width: 80%;
+        }
 
     }
 `
-
+const MoreView = styled(Link)`
+    position: absolute; 
+    left: 0; 
+    bottom: 0; 
+    width: 100%; 
+    height: 50px; 
+    background: #6A717A url(${arrowIcon}) center right 10px no-repeat; background-size:25px; 
+    color: #fff; 
+    font-size: var(--b2); 
+    text-align: right; 
+    padding-right: 35px; 
+    line-height: 50px;
+    :hover{
+        background: var(--col_acc) url(${arrowIcon}) center right 10px no-repeat;
+        background-size: 25px; 
+        transition: all .2s ease;}
+`
 
 
 
@@ -188,22 +251,6 @@ const ListWrap = styled.ul`
 // .pagination_esg{text-align: center; display: none;}
 // .pagination_esg .swiper-pagination-bullet{font-size: 0; background-color: #D9D9D9; opacity: 1;}
 // .pagination_esg .swiper-pagination-bullet-active{background-color: #000;}
-
-
-// .esg_box h4{}
-
-// .esg_box ul{}
-// .esg_box ul li{}
-// .esg_box ul li:nth-child(odd){}
-// .esg_box ul li:nth-child(even){}
-
-// .esg_box ul li:nth-child(3), .esg_box ul li:nth-child(4){margin-top: 20px;}
-
-// .esg_box span{display: inline-block;}
-// .esg_box .esglist_tit{font: var(--b1);}
-// .esg_box ul li:nth-child(odd) .esglist_tit{width: 50%;}
-// .esg_box ul li:nth-child(even) .esglist_tit{width: 70%;}
-// .esg_box .esglist_txt{font: var(--b1); font-weight: 700;}
 
 
 /* .esg_box .esglist_lv{color: var(--col_acc);}
@@ -217,6 +264,3 @@ const ListWrap = styled.ul`
 // .esg_box .esglist_lvE{color: var(--col_acc);}
 
 
-// .esg_box .esg_more{position: absolute; left: 0; bottom: 0; width: 100%; height: 50px; background: #6A717A url(../img/icon_mainesg_arrow.png) center right 10px no-repeat; background-size:25px; color: #fff; font-size: var(--b2); text-align: right; padding-right: 35px; line-height: 50px;}
-// .esg_box .esg_more:hover{background: var(--col_acc) url(../img/icon_mainesg_arrow.png) center right 10px no-repeat; background-size: 25px; transition: all .2s ease;}
-// .esg_date{font-size: 16px; color: #4F4F4F; text-align: center; margin-top: 10px;}
