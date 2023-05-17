2d158ba4a922425f959120b507545a1527edc474
ByulKim <kimbyul1225@gmail.com>
2023-05-17 20:15:23 +0900

    탭메뉴 pc버전 ui 구현

--- src/components/home/TabMenu.tsx
+++ src/components/home/TabMenu.tsx
@@ -11,6 +11,20 @@ const TabMenu = () => {
                     title="탭메뉴 슬라이더 혼합 예시"
                     color="#fff"
                 />
+                <TabWrap>
+                    <li>
+                        <button>menu 01</button>
+                    </li>
+                    <li>
+                        <button>menu 02</button>
+                    </li>
+                    <li>
+                        <button>menu 03</button>
+                    </li>
+                    <li>
+                        <button>menu 04</button>
+                    </li>
+                </TabWrap>
             </Row>
             
         </Wrap>
@@ -39,3 +53,83 @@ const Row = styled.div`
         padding: 0 20px;
     }
 `
+
+const TabWrap = styled.div`
+    display: flex; 
+    justify-content: space-around; 
+    max-width: 720px; 
+    margin: 65px auto 50px auto;
+    button{
+        color: #fff; font: var(--b1); display: block; background-color: transparent;
+    }
+    button:hover{
+        color: var(--col_acc); text-decoration: underline; transition: all .2s ease;
+    }
+    button.on{
+        color: var(--col_acc); text-decoration: underline;
+    }
+    @media screen and (max-width: 1200px) {
+        flex-wrap: wrap; 
+        justify-content: space-between; 
+        margin-top: 50px; 
+        margin-bottom: 50px; 
+        max-width: 100%;
+        button{
+            width: 100%; font-size: 20px; font-weight: 500; color: #828282; background-color: #fff; display: block; height: 80px; line-height: 80px; border: 1px solid #828282; border-radius: 5px;
+        }
+        button:hover{
+            color: #828282; text-decoration: none;
+        }
+        button.on{
+            background-color: var(--col_acc); border: 1px solid var(--col_acc); color: #fff; font-weight: 700;
+        }
+    }
+    @media screen and (max-width: 768px) {
+        margin-top: 30px; 
+        margin-bottom: 20px;
+        button{
+            font-size: 14px; font-weight: 500; height: 55px; line-height: 55px;border: 1px solid #828282; border-radius: 3px;
+        }
+        button.on{
+            font-size: 14px; font-weight: 500; height: 55px; line-height: 55px;border: 1px solid var(--col_acc); border-radius: 3px;
+        }
+    }
+`
+
+// .main_insight .tab_box{}
+
+// .main_insight .tab_box button{}
+// .main_insight .tab_box button:hover{}
+// .main_insight .tab_box button.on{}
+
+
+
+
+
+
+
+
+
+// 	.main_insight .tab_box{}
+// 	.main_insight .tab_box li{width: 48.5%; text-align: center;}
+// 	.main_insight .tab_box li:nth-child(3), .main_insight .tab_box li:nth-child(4){margin-top: 20px;}
+
+// 	.main_insight .tab_box button{}
+// 	.main_insight .tab_box button:hover{}
+// 	.main_insight .tab_box button.on{}
+
+
+
+
+
+
+
+//     .main_insight .tab_box{}
+
+// 	.main_insight .tab_box li:nth-child(3), .main_insight .tab_box li:nth-child(4){margin-top: 0;}
+// 	.main_insight .tab_box li{width: 48.5%; margin-top: 0; margin-bottom: 10px;}
+
+
+// 	.main_insight .tab_box button{}
+
+// 	.main_insight .tab_box button.on{}
--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,16 +1,18 @@
+import React from 'react';
+
 import Banner from '@/components/home/Banner';
 import ButtonType from '@/components/home/ButtonType';
 import SliderType01 from '@/components/home/SliderType01';
 import SliderType02 from '@/components/home/SliderType02';
 import SliderType03 from '@/components/home/SliderType03';
-import React from 'react';
+import TabMenu from '@/components/home/TabMenu';
 
 const Home = () => {
     return (
         <div>
             <Banner/>
             <SliderType01/>
-            <SliderType02/>
+            <TabMenu/>
             <SliderType03/>
             <ButtonType/>
         </div>
