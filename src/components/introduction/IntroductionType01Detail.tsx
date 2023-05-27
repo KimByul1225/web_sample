0d49fe9cb933266dc582d006ab7d9d480aa44a1d
ByulKim <kimbyul1225@gmail.com>
2023-05-27 19:20:25 +0900

    소개 상세페이지 모바일대응

--- src/components/introduction/IntroductionType01Detail.tsx
+++ src/components/introduction/IntroductionType01Detail.tsx
@@ -3,6 +3,8 @@ import styled from 'styled-components';
 import SubBanner from '../commons/SubBanner';
 import Container from '../layout/Container';
 import Row from '../layout/Row';
+import ImageSample from "@/resources/images/introduction/sample_04.png";
+import IconDownLoad from "@/resources/icons/introducation/icon_down.png"
 
 const IntroductionType01Detail = () => {
     return (
@@ -42,16 +44,32 @@ const IntroductionType01Detail = () => {
                             </TextBox>
                         </li>
                     </ContentsWrap>
-
-
                 </Row>
             </Container>
+
+            <AccentContainer>
+                <Row>
+                    <ImgTextComboWrap>
+                        <figure/>
+                        <ComboBoxText>
+                            <h4>Lorem ipsum</h4>
+                            <span>
+                                <p>Lorem ipsum dolor sit amet, consectetur </p>
+                                <p>ncididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis</p>
+                            </span>
+                            <BtnWrap>
+                                <button>다운로드</button>
+                            </BtnWrap>
+                        </ComboBoxText>
+                    </ImgTextComboWrap>
+                    
+                </Row>
+            </AccentContainer>
         </>
     );
 };
 
 export default IntroductionType01Detail;
-
 
 const DeatilTitle = styled.h3`
     font-size: 36px; font-weight: 400; color: #4F4F4F; padding-bottom: 100px; text-align: center; position: relative;
@@ -84,7 +102,6 @@ const HeadLine = styled.div`
         }
     }
 `
-
 const ContentsWrap = styled.ul`
     display: flex; justify-content: space-between; margin-top: 120px;
     li{
@@ -107,7 +124,6 @@ const ContentsWrap = styled.ul`
     
     }
 `
-
 const TextBox = styled.div`
     h5{
         font-size: 22px; font-weight: 500; position: relative; padding-top: 18px; margin-bottom: 20px; line-height: 28px; height: 74px; word-break: keep-all;
@@ -133,6 +149,79 @@ const TextBox = styled.div`
         }
     }
 `
+
+const AccentContainer = styled.div`
+    background-color: #F2F2F2; padding: 50px 0; margin-bottom: 120px;
+
+    @media screen and (max-width: 1200px) {
+        margin-bottom: 80px;
+    }
+    @media screen and (max-width: 768px) {
+        padding: 30px 0; margin-bottom: 50px;
+    }
+`
+
+const ImgTextComboWrap = styled.div`
+    display: flex; justify-content: space-between; margin-bottom: 0;
+    figure{
+        width: 55%; height: 400px; background: url(${ImageSample}) center no-repeat; background-size: cover;
+    }
+    @media screen and (max-width: 1200px){
+        justify-content: normal; flex-direction: column;
+        figure{
+            width: 100%;
+
+        }
+    }
+    @media screen and (max-width: 768px){
+        figure{
+            width: 100%; height: 260px;
+        }
+    }
+`
+
+const ComboBoxText = styled.div`
+    width: 45%; height: auto;  padding-left: 60px; display: flex; flex-direction: column; justify-content: center;
+    
+    h4{
+        font-size: 28px; font-weight: 700; color: #313131; letter-spacing: -0.05em; word-break: keep-all; line-height: 48px; padding-top: 18px; padding-bottom: 40px; position: relative; border-bottom: 1px solid #E0E0E0; 
+    }
+    h4::before{
+        content: ''; display: block; position: absolute; top: 0; left: 0; width: 8px; height: 8px; border-radius: 2px; background-color: var(--col_acc);
+    }
+    span{
+        display: block; font-size: 16px; border: none; margin: 60px 0 40px 0;
+    }
+    p{
+        color: #4F4F4F; font-size: 16px; font-weight: 400; word-break: keep-all; line-height: 27px;
+    }
+
+    @media screen and (max-width: 1200px) {
+        width: 100%; padding-left: 0; height: auto; margin-top: 40px;
+        span{
+            padding-top: 0; margin: 20px 0;
+        }
+    }
+    @media screen and (max-width: 768px) {
+        h4{
+            font-size: 18px; line-height: 30px; padding-bottom: 10px;
+        }
+        p{
+            font-size: 14px; line-height: 24px;
+        }
+    }
+`
+
+const BtnWrap = styled.div`
+    margin-top: 0; 
+    button{
+        width: auto; height: auto; background-color: transparent; border: none; font-size: 18px; font-weight: 500; padding-left: 30px; background: url(${IconDownLoad}) center left no-repeat; background-size: 24px; line-height: 24px; text-align: left; border-radius: 0; color: var(--col_acc); 
+    }
+    @media screen and (max-width: 1200px){
+        margin-top: 40px;
+    }
+`
+
 
 
 
